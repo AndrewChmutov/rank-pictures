897daf2f69790799e7c9eff935b7736b78db1dd7
Andrew Chmutov <ulman.andre@gmail.com>
2023-08-28 22:56:49 +0200

    Adding fields to new menu

--- include/rank_menu.hpp
+++ include/rank_menu.hpp
@@ -2,9 +2,34 @@
 
 // Custom libraries
 #include "base_menu.hpp"
+#include "transition_state.hpp"
+#include "picture_record.hpp"
+
+// SDL libraries
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 class RankMenu : public BaseMenu {
+    SDL_Texture* picture;
+    SDL_Texture* name,* wins,* winrate,* total;
+    TTF_Font* font;
+    
+    TransitionState transitionState;
+    float transitionProgress;
+    float delta;
+
+    PictureRecord& pictures;
+
+    void startTransitionIn();
+    void startTransitionOut();
+
+    void updateTransitionIn();
+    void updateTransitionOut();
 
 public:
+    RankMenu(PictureRecord& picture);
+
     virtual MenuEvent handleSpecificEvent(const SDL_Event& event, Screen& screen) override;
+
+    virtual void update(const Screen& screen) override;
 };
