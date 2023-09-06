59ec1bf6cbd0a18f2824d127ac74014f112d78af
Andrew Chmutov <ulman.andre@gmail.com>
2023-09-06 23:38:28 +0200

    Comment rank header

--- include/rank_menu.hpp
+++ include/rank_menu.hpp
@@ -13,16 +13,23 @@
 #include <SDL_render.h>
 
 class RankMenu : public BaseMenu {
+    // Pictures
+    std::vector<PictureRecord>& pictures;
+    int index;
+
+
+    // Labels //
+
     // Font fot labels
     std::string pathToFont;
+    int nameFont;
+    int otherFont;
 
     // Name
-    int nameFont;
     SDL_Rect nameRect;
     SDL_Texture* nameTexture;
 
-    int otherFont;
-
+    // # Index
     SDL_Rect indexRect;
     std::string indexText;
     SDL_Texture* indexTexture;
@@ -45,66 +52,98 @@ class RankMenu : public BaseMenu {
     // Picture
     SDL_Texture* pictureTexture;
     SDL_Rect pictureRect;
-    int boxW, boxH;
 
+    // Technical details //
+
+    // Borders
     SDL_Rect borders;
+    int boxW, boxH;
     float displacement;
 
+    // Transition
     TransitionState transitionState;
     float transitionProgress, delta;
 
-    std::vector<PictureRecord>& pictures;
-    int index, size;
-
-    MenuEvent eventTransition;
+    // Return value
     MenuEvent toReturn;
 
+    // Loads all textures
     void loadEntities(Screen& screen);
-    void loadName(Screen& screen);
-    void loadPicture(Screen& screen);
-    void loadIndex(Screen& screen);
-    void loadWins(Screen& screen);
-    void loadWinrate(Screen& screen);
-    void loadTotal(Screen& screen);
+    void loadName(Screen& screen);      // name texture
+    void loadPicture(Screen& screen);   // picture texture
+    void loadIndex(Screen& screen);     // index texture
+    void loadWins(Screen& screen);      // wins texture
+    void loadWinrate(Screen& screen);   // winrate texture
+    void loadTotal(Screen& screen);     // total texture
 
+    // Used for loading any label
     void loadLabel(Screen& screen, SDL_Texture** tempTexture, const std::string& toShow);
 
-    void freeTexture(SDL_Texture** texture);
+    // Free all textures
     void freeEntities();
+    void freeTexture(SDL_Texture** texture);    // Free texture if needed
 
-    void startTransitionIn(TransitionState state);
+    // Transitions //
+
+    // Starts transition in
+    // Changes transition state
+    void startTransitionIn(TransitionState state);  
+
+    // Starts transition out
+    // Changes transtion state
     void startTransitionOut(TransitionState state);
 
+    // Starts transition to left
+    // - Sets the parameters for transition
+    // - Makes sure if index >= 0
     void startTransitionLeftIn();
+
+    // Starts transition to right
+    // - Sets the parameters for transition
+    // - Makes sure if index < size of vector of pictures
     void startTransitionRightIn();
 
+    // Updates positions for transition in
     void updateTransitionIn();
     void updateTransitionDefaultIn();
     void updateTransitionLeftIn();
     void updateTransitionRightIn();
 
+    // Updates positions for transition out
     void updateTransitionOut();
     void updateTransitionDefaultOut();
     void updateTransitionLeftOut();
     void updateTransitionRightOut();
 
+    // Change information according to 
     void updateInfo(Screen& screen, TransitionState previous);
 
+    // Renders transition in
     void renderTransitionIn();
+
+    // Renders transition out
     void renderTransitionOut();
 
+    // Starts transition to left picture
+    // Changes transition state
     void toLeft();
+
+    // Starts transition to right picture
+    // Changes transition state
     void toRight();
 
+    // Handle menu-specific events:
+    // - SPACE key 
+    virtual MenuEvent handleSpecificEvent(const SDL_Event& event, Screen& screen) override;
 public:
     RankMenu(Screen& screen, std::vector<PictureRecord>& pictures, std::string pathToFont);
 
     virtual MenuEvent handleEvents(Screen& screen) override;
 
-    virtual MenuEvent handleSpecificEvent(const SDL_Event& event, Screen& screen) override;
-
+    // Update menu
     virtual void update(Screen& screen) override;
 
+    // Render menu
     virtual void render(Screen& screen) override;
 
     virtual ~RankMenu() override;
